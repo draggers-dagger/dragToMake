dd325181d750325569ada7ce65180229fdbc7436
suicide11 <b117045@iiit-bh.ac.in>
2019-11-12 14:41:24 +0530

    console added|

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -18,10 +18,10 @@ const styles = {
 const Container = ({ ...props }) => {
   const [boxesArray, setBoxesFunc] = useState([
     // { id: 'b', top: 20, left: 20, title: <DatePicker /> },
-    { id: 'c', top: 220, left: 30, title: <Button>haha</Button> },
+    // { id: 'c', top: 220, left: 30, title: <Button>haha</Button> },
   ])
 
-  useEffect(() => {
+  useEffect( () => {
     if (props.widget) {
       console.log(props.type, "widgettt")
       const components = {
@@ -31,6 +31,21 @@ const Container = ({ ...props }) => {
       setBoxesFunc([...boxesArray, { id: id, top: 0, left: 0, title: props.widget, type: props.type }])
     }
   }, [props.widget]);
+
+  useEffect( () => {
+    if (boxesArray) {
+      props.handleAllWidget(boxesArray)
+    }
+  }, [boxesArray]);
+
+  useEffect(()=>{
+    if(props.deleteWidget){
+     setBoxesFunc( boxesArray.filter(function(widget) { 
+       console.log(props.deleteWidget.id)
+        return widget.id !== props.deleteWidget.id 
+    }))
+    }
+  },[props.deleteWidget])
 
   const [, drop] = useDrop({
     accept: [ItemTypes.BOX, ItemTypes.BUTTON],
---
+++ src/components/layout/console.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { Icon, Button } from 'antd'
+import ConsoleWidget from './consoleWidget'
+class Console extends Component {
+
+    handleWidget = () => {
+        return this.props.AllWidget.map(Widget =>
+
+            <ConsoleWidget handleDeleteWidget={this.props.handleDeleteWidget} key={Widget.id} Widget={Widget}></ConsoleWidget>
+        )
+    }
+
+    render() {
+        return (
+
+            <div>
+                <div className="console-btn">
+                    <Button data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" type="primary" style={{ padding: "0 40px" }} >
+                        console
+                </Button>
+
+                </div>
+                <div class="collapse" id="collapseExample" className="console-body">
+                    <center style={{ color: "white" }}> Click console to close this pane </center>
+                    {this.handleWidget()}
+                    <br></br>
+
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default Console
